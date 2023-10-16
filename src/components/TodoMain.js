3725050b6c743d35a8425915a60c9ffc92a39076
Araf <arafathossain.baiust@gmail.com>
2023-10-16 10:26:25 +0600

    first commit

---
+++ src/components/TodoMain.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const TodoMain = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default TodoMain;
+
+//Todo - TodoMain
+//TodoForm - TodoList
+//TodoWrapper - TodoWrap
+//
---
+++ src/components/TodoWrap.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TodoWrap = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default TodoWrap;
